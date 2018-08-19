9ddbeadd400c31c1a17d5044ca9c009fb21da886
hwalborn <hwalborn@gmail.com>
2018-08-19 17:39:56 -0400

    cleaned up countdown_list so that it doesn't do nested loops and countdown_controller returns sorted list

--- app/controllers/countdown_controller.rb
+++ app/controllers/countdown_controller.rb
@@ -1,6 +1,8 @@
 class CountdownController < ApplicationController
   def index
     countdown_list = CountdownList.new()
-    render json: countdown_list.upcoming_departures
+    render json: countdown_list.upcoming_departures.sort_by! {
+      |dept| dept.arrival
+    }
   end
 end
--- app/models/countdown_list.rb
+++ app/models/countdown_list.rb
@@ -3,7 +3,7 @@ class CountdownList
   attr_accessor :upcoming_departures
   def initialize
     # grab our data from gtfs
-    data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=#{ENV["api_key"]}&feed_id=31"))
+    data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=#{ENV["api_key"]}&feed_id=1"))
     # use the gtfs-realtime-bindings gem to decode it to a feed list
     @feed_list = Transit_realtime::FeedMessage.decode(data).entity
     # empty array to use later
@@ -17,17 +17,17 @@ class CountdownList
       # optional for gtfs, so we need to make sure it has it
       if entity.field?(:trip_update)
         # get our stop time data
-        train_stops = entity[:trip_update][:stop_time_update]
-        # iterate over stops and find GreenPoint ave to add to our list
-        train_stops.each do |stop|
-          # looking just for the GreenPoint Ave (stop_id = 239) stops...
-          if(stop[:stop_id].include? "G26")
-            # make a new instance of our DepartureModel... So that we know
-            # which direction and the arrival time is converted from POSIX
-            upcoming_departure = DepartureModel.new(stop[:arrival][:time], stop[:stop_id])
-            @upcoming_departures.push(upcoming_departure)
-          end
+        departures = entity[:trip_update][:stop_time_update]
+        .select do |stop|
+          # looking just for the GreenPoint Ave (stop_id = G26) stops...
+          stop[:stop_id].include? "239"
         end
+        .map do |stop|
+          # make a new instance of our DepartureModel... So that we know
+          # which direction and the arrival time is converted from POSIX
+          DepartureModel.new(stop[:arrival][:time], stop[:stop_id])
+        end
+        @upcoming_departures.concat(departures)
       end
     end
   end
